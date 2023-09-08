ed3db284de7962493d4c5f3fc2c422bd31c16851
minzynotes <wangminzy1@gmail.com>
2023-09-08 09:39:12 +0200

    update while loop

--- src/do-you-want-to-stop.py
+++ src/do-you-want-to-stop.py
@@ -1,3 +1,22 @@
 
 # You should write a loop around this question, and keep asking until
 # the answer is "yes" (lower case).
+# input('Do you want to stop?')
+#n=0
+#while n<7:
+#    print ("Hello, World!")
+#    n += 1
+
+a = int(input('How many times?'))
+n = 0
+
+while n <= a:
+    user_input = input("Do you want to stop? (yes/break): ")
+    if user_input.lower() == 'yes':
+        break
+    elif user_input.lower() == "break":
+         print("Hello, World!")
+         n += 1
+    else:
+        print('warning')
+        n +=1
--- src/print-1-10.py
+++ src/print-1-10.py
@@ -1,2 +1,4 @@
 
 # Print the numbers described in the exercise
+for x in range (1,11):
+    print(x)
